af61887358719dfffd44f8248f85d5e22d80b6d3
Maaz Tariq <maazt254@gmail.com>
2025-12-26 17:51:36 +0530

    Don't use old react CRA instead use react+vite, btw project initialisation is completed

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+      <div className="container mt-5">
+      <h1 className="text-primary">hello world!!!</h1>
+      <button className="btn btn-success">Click me</button>
+    </div>
+  )
+}
+
+export default App
