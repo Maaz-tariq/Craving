003dda01f9a28a35f26ff626a3ae9ebeaf14772e
Maaz Tariq <maazt254@gmail.com>
2026-01-27 23:58:43 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 console.log("🔥 index.js loaded")
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 const mongoDBconnect = require('./db');
 const cors = require('cors');
 
